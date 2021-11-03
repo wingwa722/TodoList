0498a1d36e6e7b517404e2bcb2cefe49d052ff10
wingwa722 <wingwa722@gmail.com>
2021-11-03 09:38:01 +0800

    [todolist][Wing]minor adjustment after code review

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,20 +1,19 @@
 import '../style/TodoItem.css'
 import { useDispatch } from "react-redux";
 
-const TodoItem = ({todo}) => {
+const TodoItem = ({ todo }) => {
     const todoStatus = todo.done ? 'boxDone' : 'box'
     const dispatch = useDispatch()
 
-    return (<div 
-        onClick = {() => dispatch({ type: 'todo/updateStatus', payload: todo.id })}
-        className={todoStatus}>
+    return (
+        <div onClick={() => dispatch({ type: 'todo/updateStatus', payload: todo.id })}
+            className={todoStatus}>
         {todo.text}
-        <span
-            onClick = {() => dispatch({ type: 'todo/delete', payload: todo.id })}
-            className='delete'>
-        X
-        </span>
-    </div>
+            <span onClick={() => dispatch({ type: 'todo/delete', payload: todo.id })}
+                className='delete'>
+                X
+            </span>
+        </div>
     )
 }
 export default TodoItem;
